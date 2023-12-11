cbbe8ab635cc8321fd9eed3cd7c8df203c771199
“ShD <bolatuly.shaken@gmail.com>
2023-12-12 00:46:41 +0600

    linux commit

---
+++ market/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template
+
+from market import app
+from market.forms import RegisterForm
+from market.models import Item
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
+
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
